04a37da6eff58bd71e7bf4a35b4a16e093beee5d
shiheng <Xheng4@hotmail.com>
2017-03-31 13:24:36 +0800

    个人中心布局调整，个人中心调用方法显示头像，昵称和用户名。

--- WeChat/src/cn/ucai/superwechat/ui/fragment/PersonalCenterFragment.java
+++ WeChat/src/cn/ucai/superwechat/ui/fragment/PersonalCenterFragment.java
@@ -1,19 +1,37 @@
 package cn.ucai.superwechat.ui.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.widget.MFGT;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersonalCenterFragment extends Fragment {
 
+
+    @BindView(R.id.iv_avatar_pc)
+    ImageView mIvAvatarPc;
+    @BindView(R.id.tv_nick_pc)
+    TextView mTvNickPc;
+    @BindView(R.id.tv_username_pc)
+    TextView mTvUsernamePc;
 
     public PersonalCenterFragment() {
         // Required empty public constructor
@@ -24,7 +42,42 @@ public class PersonalCenterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_center, container, false);
+        View view = inflater.inflate(R.layout.fragment_personal_center, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+        String username = EMClient.getInstance().getCurrentUser();
+        if (username != null) {
+            mTvUsernamePc.setText(username);
+            EaseUserUtils.setWeChatUserNick(username, mTvNickPc);
+            EaseUserUtils.setWeChatUserAvatar(getContext(), username, mIvAvatarPc);
+        }
+    }
+
+    @OnClick({R.id.pc_profile_center, R.id.pc_photo_item, R.id.pc_collect_item, R.id.pc_money_item, R.id.pc_smail_item, R.id.pc_setting_item})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.pc_profile_center:
+                
+                break;
+            case R.id.pc_photo_item:
+                break;
+            case R.id.pc_collect_item:
+                break;
+            case R.id.pc_money_item:
+                break;
+            case R.id.pc_smail_item:
+                break;
+            case R.id.pc_setting_item:
+                break;
+        }
+    }
 }
